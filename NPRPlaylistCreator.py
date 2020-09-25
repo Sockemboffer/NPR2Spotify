41cf8a46ca2754d41c287b373750450b4ea62c8b
Jeffrey Hofer <jeffrey.d.hofer@pm.me>
2020-09-24 21:23:45 -0500

    Added requests retry function, updated comments.

--- NPRPlaylistCreator.py
+++ NPRPlaylistCreator.py
@@ -1,25 +1,31 @@
 import json
 import base64
+import logging
 import requests
 import datetime
 from ResponsesHandle import ResponseException
 from secrets import spotify_user_id, spotipyUserToken
-# todo: retry on request submissions
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 class NPRPlaylistCreator:
 
     def CreatePlaylist(playlistName):
+        requestSession = requests.Session()
+        retries = Retry(total=3, backoff_factor=1, status_forcelist=[ 502, 503, 504 ])
+        requestSession.mount('https://api.spotify.com/', HTTPAdapter(max_retries=retries))
         # Playlist max character name limit is 100
         request_body = json.dumps({"name": playlistName, "public": False})
         query = "https://api.spotify.com/v1/users/{}/playlists".format(spotify_user_id)
-        response = requests.post(query, data=request_body, headers={"Content-Type": "application/json", "Authorization": "Bearer {}".format(spotipyUserToken)})
-        if response.status_code != 201:
-            raise ResponseException(response.status_code)
+        response = requestSession.post(query, data=request_body, headers={"Content-Type": "application/json", "Authorization": "Bearer {}".format(spotipyUserToken)})
         response_json = response.json()
         print("-- Playlist created.")
         return response_json
 
     def AddTracksToPlaylist(searchedTracks, playlistID):
+        requestSession = requests.Session()
+        retries = Retry(total=3, backoff_factor=1, status_forcelist=[ 502, 503, 504 ])
+        requestSession.mount('https://api.spotify.com/', HTTPAdapter(max_retries=retries))
         tracksURIs = list()
         urisData = dict()
         for track in searchedTracks:
@@ -28,29 +34,28 @@ class NPRPlaylistCreator:
         query = "https://api.spotify.com/v1/playlists/{}/tracks".format(playlistID)
         urisData["uris"] = tracksURIs
         request_data = json.dumps(urisData)
-        response = requests.post(query, request_data, headers={"Content-Type": "application/json", "Authorization": "Bearer {}".format(spotipyUserToken)})
-        if response.status_code != 201:
-            raise ResponseException(response.status_code)
+        response = requestSession.post(query, request_data, headers={"Content-Type": "application/json", "Authorization": "Bearer {}".format(spotipyUserToken)})
         print("-- Playlist tracks added.")
 
     def AddCoverArtToPlaylist(searchedTracks, day, playlistID):
+        requestSession = requests.Session()
+        retries = Retry(total=3, backoff_factor=1, status_forcelist=[ 502, 503, 504 ])
+        requestSession.mount('https://api.spotify.com/', HTTPAdapter(max_retries=retries))
         encoded_string = NPRPlaylistCreator.GetNewCover(searchedTracks, day)
         query = "https://api.spotify.com/v1/users/{}/playlists/{}/images".format(spotify_user_id, playlistID) 
-        response = requests.put(query, encoded_string, headers={"Authorization": "Bearer {}".format(spotipyUserToken), "Content-Type": "image/jpeg"})
-        if response.status_code != 202:
-            raise ResponseException(response.status_code)
+        response = requestSession.put(query, encoded_string, headers={"Authorization": "Bearer {}".format(spotipyUserToken), "Content-Type": "image/jpeg"})
         print("-- Playlist cover image added.")
 
     def UpdatePlaylistDescription(searchedTracks, playlistID, nprURL):
-        # 300 character limit playlist desciption
-        # returns response ok if over limit, but no description will be made.
+        requestSession = requests.Session()
+        retries = Retry(total=3, backoff_factor=1, status_forcelist=[ 502, 503, 504 ])
+        requestSession.mount('https://api.spotify.com/', HTTPAdapter(max_retries=retries))
         missedTracksList = list()
-        # Removing exessive characters so description has more
+        # Removing exessive link-guts so description has more to work with
         nprURL = nprURL.partition("?")
         for missedTrack in searchedTracks:
             if missedTrack["Found Match Type"] == "NoHit" or missedTrack["Found Match Type"] == "HitButNoMatch":
                 missedTracksList.append(missedTrack)
-        #print(missedTracksList)
         if missedTracksList != None:
             newDescription = dict()
             newDescription["description"] = str(nprURL[0]) + " [MISSING:" + str(len(missedTracksList)) + "]"
@@ -60,7 +65,9 @@ class NPRPlaylistCreator:
                 else:
                     newDescription["description"] += " Song: " + track["NPR Track Name"] + " by: " + ", ".join(track["NPR Artist Name"]) + ","
             newDescription["description"] += " {Checked: " + str(datetime.datetime.now().__format__("%Y-%m-%d")) + "}--Send fixes to: addy@something.com"
+            # 300 character limit playlist desciption
             # Check if description exceeds character limit so we can truncate
+            # returns response ok if over limit, but no description will be made.
             if len(newDescription["description"]) > 300:
                 newDescription["description"] = newDescription["description"][:300]
                 newEndingDescription =  "<...too many missing.>" + "{Checked: " + str(datetime.datetime.now().__format__("%Y-%m-%d")) + "}--Send fixes to: addy@something.com"
@@ -70,9 +77,7 @@ class NPRPlaylistCreator:
             newDescription = dict()
             newDescription["description"] = str(nprURL) + " [ALL TRACKS FOUND!] {Checked: " + str(datetime.datetime.now().__format__("%Y-%m-%d")) + "}--Send fixes to: addy@something.com"
         query = "https://api.spotify.com/v1/playlists/{}".format(playlistID)
-        response = requests.put(query, json.dumps(newDescription), headers={"Content-Type": "application/json", "Authorization": "Bearer {}".format(spotipyUserToken)})
-        if response.status_code != 200:
-            raise ResponseException(response.status_code)
+        response = requestSession.put(query, json.dumps(newDescription), headers={"Content-Type": "application/json", "Authorization": "Bearer {}".format(spotipyUserToken)})
         print("!! Truncated description.")
 
     def GetNewCover(searchedTracks, day):
