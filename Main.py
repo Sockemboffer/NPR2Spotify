cd757794fb32b439ece6524e93766998a8942dc8
Jeffrey Hofer <jeffrey.d.hofer@pm.me>
2021-05-17 11:40:31 -0500

    2000 and 2001

--- Main.py
+++ Main.py
@@ -12,33 +12,31 @@ from NPRPlaylistCreator import NPRPlaylistCreator
 
 # Jan 1th 2000's seems to be when some interlude data is being documented
 # Used to create complete years
-editionYear = 2000
+editionYear = 2001
 editionDayData = list()
 editionYearLinkCache = NPRPageParser.LoadJSONFile("MoWeEd Article Link Cache/" + str(editionYear) + " MoWeEd Article Link Cache.json")
 for month, daylinks in editionYearLinkCache.items():
-    if month == "12":
-        for idx, link in enumerate(daylinks):
-            if idx >= 20:
-                nprSpotifySearch = NPRSpotifySearch()
-                nprPlaylistCreator = NPRPlaylistCreator()
-                nprPageParser = NPRPageParser()
-                requestedHTML = NPRPageParser.RequestURL(link)
-                selectedHTML = NPRPageParser.SelectStory(requestedHTML.text) # select the returned HTML
-                editionDayData.append(NPRPageParser.GetEditionData(link, selectedHTML)) # get various article data from this day
-                for story in selectedHTML.xpath('.//div[@id="story-list"]/*'):
-                    if story.attrib['class'] == 'rundown-segment':
-                        editionDayData.append(NPRPageParser.GetArticleInfo(story))
-                    elif story.attrib['class'] == 'music-interlude responsive-rundown':
-                        for songMETA in story.xpath('.//div[@class="song-meta-wrap"]'):
-                            interlude = dict()
-                            interlude["MoWeEd Track"] = NPRPageParser.GetInterludeSongName(songMETA)
-                            interlude["MoWeEd Artists"] = NPRPageParser.GetInterludeArtistNames(songMETA)
-                            editionDayData.append(interlude)
-                            print(json.dumps(interlude, indent=4, sort_keys=True, ensure_ascii=False))
-                nprPageParser.SaveJSONFile(editionDayData)
-                print("Finished {0}\n".format(editionDayData[0]['Page Link']))
-                editionDayData.clear()
-                time.sleep(1) # Don't hammer their server
+    for idx, link in enumerate(daylinks):
+        nprSpotifySearch = NPRSpotifySearch()
+        nprPlaylistCreator = NPRPlaylistCreator()
+        nprPageParser = NPRPageParser()
+        requestedHTML = NPRPageParser.RequestURL(link)
+        selectedHTML = NPRPageParser.SelectStory(requestedHTML.text) # select the returned HTML
+        editionDayData.append(NPRPageParser.GetEditionData(link, selectedHTML)) # get various article data from this day
+        for story in selectedHTML.xpath('.//div[@id="story-list"]/*'):
+            if story.attrib['class'] == 'rundown-segment':
+                editionDayData.append(NPRPageParser.GetArticleInfo(story))
+            elif story.attrib['class'] == 'music-interlude responsive-rundown':
+                for songMETA in story.xpath('.//div[@class="song-meta-wrap"]'):
+                    interlude = dict()
+                    interlude["MoWeEd Track"] = NPRPageParser.GetInterludeSongName(songMETA)
+                    interlude["MoWeEd Artists"] = NPRPageParser.GetInterludeArtistNames(songMETA)
+                    editionDayData.append(interlude)
+                    print(json.dumps(interlude, indent=4, sort_keys=True, ensure_ascii=False))
+        nprPageParser.SaveJSONFile(editionDayData)
+        print("Finished {0}\n".format(editionDayData[0]['Page Link']))
+        editionDayData.clear()
+        time.sleep(1.5) # Don't hammer their server
 
 # # Read in Article Data and create a playlist out of it
 #   if (dayDetails['Day'] == 'Saturday') or (dayDetails['Day'] == 'Sunday'):
