fd84b98d18b870d062e4b695a11c0f7dc10eb6ab
Jeffrey Hofer <jeffrey.d.hofer@pm.me>
2021-05-17 10:28:45 -0500

    Adding in month and day indexing. 2000 12 01 was first day interlude data starts to show up on their pages.

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import enum
 import os
 import time
 import json
@@ -9,33 +10,35 @@ from NPRPlaylistCreator import NPRPlaylistCreator
 # # # Create a json file for each year of day links (only need to run one time)
 # NPRPageParser.NPRArticleLinkCacheCreator(2018) # 1996 - 2020
 
-# July 28th 2000's seems to be when some interlude data is being documented
+# Jan 1th 2000's seems to be when some interlude data is being documented
 # Used to create complete years
-editionYear = 1999
+editionYear = 2000
 editionDayData = list()
 editionYearLinkCache = NPRPageParser.LoadJSONFile("MoWeEd Article Link Cache/" + str(editionYear) + " MoWeEd Article Link Cache.json")
 for month, daylinks in editionYearLinkCache.items():
-    for url in daylinks:
-        nprSpotifySearch = NPRSpotifySearch()
-        nprPlaylistCreator = NPRPlaylistCreator()
-        nprPageParser = NPRPageParser()
-        requestedHTML = NPRPageParser.RequestURL(url)
-        selectedHTML = NPRPageParser.SelectStory(requestedHTML.text) # select the returned HTML
-        editionDayData.append(NPRPageParser.GetEditionData(url, selectedHTML)) # get various article data from this day
-        for story in selectedHTML.xpath('.//div[@id="story-list"]/*'):
-            if story.attrib['class'] == 'rundown-segment':
-                editionDayData.append(NPRPageParser.GetArticleInfo(story))
-            elif story.attrib['class'] == 'music-interlude responsive-rundown':
-                for songMETA in story.xpath('.//div[@class="song-meta-wrap"]'):
-                    interlude = dict()
-                    interlude["MoWeEd Track"] = NPRPageParser.GetInterludeSongName(songMETA)
-                    interlude["MoWeEd Artists"] = NPRPageParser.GetInterludeArtistNames(songMETA)
-                    editionDayData.append(interlude)
-                    print(json.dumps(interlude, indent=4, sort_keys=True, ensure_ascii=False))
-        nprPageParser.SaveJSONFile(editionDayData)
-        print("Finished {0}\n".format(editionDayData[0]['Page Link']))
-        editionDayData.clear()
-        time.sleep(1) # Don't hammer their server
+    if month == "12":
+        for idx, link in enumerate(daylinks):
+            if idx >= 20:
+                nprSpotifySearch = NPRSpotifySearch()
+                nprPlaylistCreator = NPRPlaylistCreator()
+                nprPageParser = NPRPageParser()
+                requestedHTML = NPRPageParser.RequestURL(link)
+                selectedHTML = NPRPageParser.SelectStory(requestedHTML.text) # select the returned HTML
+                editionDayData.append(NPRPageParser.GetEditionData(link, selectedHTML)) # get various article data from this day
+                for story in selectedHTML.xpath('.//div[@id="story-list"]/*'):
+                    if story.attrib['class'] == 'rundown-segment':
+                        editionDayData.append(NPRPageParser.GetArticleInfo(story))
+                    elif story.attrib['class'] == 'music-interlude responsive-rundown':
+                        for songMETA in story.xpath('.//div[@class="song-meta-wrap"]'):
+                            interlude = dict()
+                            interlude["MoWeEd Track"] = NPRPageParser.GetInterludeSongName(songMETA)
+                            interlude["MoWeEd Artists"] = NPRPageParser.GetInterludeArtistNames(songMETA)
+                            editionDayData.append(interlude)
+                            print(json.dumps(interlude, indent=4, sort_keys=True, ensure_ascii=False))
+                nprPageParser.SaveJSONFile(editionDayData)
+                print("Finished {0}\n".format(editionDayData[0]['Page Link']))
+                editionDayData.clear()
+                time.sleep(1) # Don't hammer their server
 
 # # Read in Article Data and create a playlist out of it
 #   if (dayDetails['Day'] == 'Saturday') or (dayDetails['Day'] == 'Sunday'):
