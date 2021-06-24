cb8881e7d164e6679f4049fedf3c9ff89f8eb755
Jeffrey Hofer <jeffrey.d.hofer@pm.me>
2021-06-23 21:14:06 -0500

    Created 2021 link cache up to today June 23rd

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ from NPRSpotifySearch import NPRSpotifySearch
 from NPRPlaylistCreator import NPRPlaylistCreator
 
 # # Used to parse a range of dates, load the json for those days, and make playlists on spotify
-# startDate = datetime(2015, 8, 13) # check 9 to 3, 28 for missing track playlists
+# startDate = datetime(2020, 6, 14) # check 9 to 3, 28 for missing track playlists
 # projectName = "MoWeEd"
 # weekendEdition = "Weekend Edition"
 # morningEdition = "Morning Edition"
@@ -19,7 +19,7 @@ from NPRPlaylistCreator import NPRPlaylistCreator
 # nprPageParser = NPRPageParser()
 # spotifyTracks = list()
 # startTime = datetime.now()
-# while startDate != datetime(2016, 1, 1):
+# while startDate != datetime(2021, 1, 1):
 #     processedTime = datetime.now()
 #     projectPath = projectName + " Article Data/{0}/{1}/".format(startDate.year, startDate.strftime("%m"))
 #     morningEditionFileName = projectName + " {0} {1} {2}".format(startDate.strftime("%Y-%m-%d"), startDate.strftime("%a"), "Morning Edition")
@@ -101,46 +101,46 @@ from NPRPlaylistCreator import NPRPlaylistCreator
 #     print("Current time: {0}, Process time: {1}, Run time: {2}".format(timestampStr, datetime.now() - processedTime, datetime.now() - startTime))
 #     print("\n")
 
-# Weekend edition shows up 1998 Jan
-# July 25th 2000's seems to be when some morning edition interlude data is being documented
-# Used to create json output for each day with various article and track data
-# TODO catch and handle badgateway error that happens seldomly
-editionStartYear = 2016
-editionDayData = list()
-projectName = "MoWeEd"
-editionYearLinkCache = NPRPageParser.LoadJSONFile(projectName + " Article Link Cache/" + str(editionStartYear) + " " + projectName + " Article Link Cache.json")
-for month, daylinks in editionYearLinkCache.items():
-    for idx, link in enumerate(daylinks):
-        ### if int(month) <= 3:
-        nprSpotifySearch = NPRSpotifySearch()
-        nprPlaylistCreator = NPRPlaylistCreator()
-        nprPageParser = NPRPageParser()
-        requestedHTML = NPRPageParser.RequestURL(link)
-        selectedHTML = NPRPageParser.SelectStory(requestedHTML.text) # select the returned HTML
-        editionDayData.append(NPRPageParser.GetEditionData(link, selectedHTML)) # get various article data from this day
-        for story in selectedHTML.xpath('.//div[@id="story-list"]/*'):
-            if story.attrib['class'] == 'rundown-segment':
-                editionDayData.append(NPRPageParser.GetArticleInfo(story))
-            elif story.attrib['class'] == 'music-interlude responsive-rundown':
-                for songMETA in story.xpath('.//div[@class="song-meta-wrap"]'):
-                    interlude = dict()
-                    interlude["MoWeEd Track"] = NPRPageParser.GetInterludeSongName(songMETA)
-                    interlude["MoWeEd Artists"] = NPRPageParser.GetInterludeArtistNames(songMETA)
-                    editionDayData.append(interlude)
-                    print(json.dumps(interlude, indent=4, sort_keys=True, ensure_ascii=False))
-        editionYear = editionDayData[0]['Date Numbered'][0:4]
-        editionMonth = editionDayData[0]['Date Numbered'].partition("-")[2].partition("-")[0]
-        editionDate = editionDayData[0]['Date Numbered']
-        editionDay = editionDayData[0]['Day']
-        editionEdition = editionDayData[0]["Edition"][0:15]
-        projectName = "MoWeEd"
-        fileType = ".json"
-        fileName = projectName + " " + editionDate + " " + editionDay + " " + editionEdition + fileType
-        directoryPath = "MoWeEd Article Data/{0}/{1}/".format(editionYear, editionMonth)
-        nprPageParser.SaveJSONFile(editionDayData, directoryPath, fileName)
-        print("Finished {0}\n".format(editionDayData[0]['Page Link']))
-        editionDayData.clear()
-        time.sleep(1) # Don't hammer their server
+# # Weekend edition shows up 1998 Jan
+# # July 25th 2000's seems to be when some morning edition interlude data is being documented
+# # Used to create json output for each day with various article and track data
+# # TODO catch and handle badgateway error that happens seldomly
+# editionStartYear = 2021
+# editionDayData = list()
+# projectName = "MoWeEd"
+# editionYearLinkCache = NPRPageParser.LoadJSONFile(projectName + " Article Link Cache/" + str(editionStartYear) + " " + projectName + " Article Link Cache.json")
+# for month, daylinks in editionYearLinkCache.items():
+#     for idx, link in enumerate(daylinks):
+#         # if int(month) <= 5:
+#         nprSpotifySearch = NPRSpotifySearch()
+#         nprPlaylistCreator = NPRPlaylistCreator()
+#         nprPageParser = NPRPageParser()
+#         requestedHTML = NPRPageParser.RequestURL(link)
+#         selectedHTML = NPRPageParser.SelectStory(requestedHTML.text) # select the returned HTML
+#         editionDayData.append(NPRPageParser.GetEditionData(link, selectedHTML)) # get various article data from this day
+#         for story in selectedHTML.xpath('.//div[@id="story-list"]/*'):
+#             if story.attrib['class'] == 'rundown-segment':
+#                 editionDayData.append(NPRPageParser.GetArticleInfo(story))
+#             elif story.attrib['class'] == 'music-interlude responsive-rundown':
+#                 for songMETA in story.xpath('.//div[@class="song-meta-wrap"]'):
+#                     interlude = dict()
+#                     interlude["MoWeEd Track"] = NPRPageParser.GetInterludeSongName(songMETA)
+#                     interlude["MoWeEd Artists"] = NPRPageParser.GetInterludeArtistNames(songMETA)
+#                     editionDayData.append(interlude)
+#                     print(json.dumps(interlude, indent=4, sort_keys=True, ensure_ascii=False))
+#         editionYear = editionDayData[0]['Date Numbered'][0:4]
+#         editionMonth = editionDayData[0]['Date Numbered'].partition("-")[2].partition("-")[0]
+#         editionDate = editionDayData[0]['Date Numbered']
+#         editionDay = editionDayData[0]['Day']
+#         editionEdition = editionDayData[0]["Edition"][0:15]
+#         projectName = "MoWeEd"
+#         fileType = ".json"
+#         fileName = projectName + " " + editionDate + " " + editionDay + " " + editionEdition + fileType
+#         directoryPath = "MoWeEd Article Data/{0}/{1}/".format(editionYear, editionMonth)
+#         nprPageParser.SaveJSONFile(editionDayData, directoryPath, fileName)
+#         print("Finished {0}\n".format(editionDayData[0]['Page Link']))
+#         editionDayData.clear()
+#         time.sleep(1) # Don't hammer their server
 
-# # Create a json file for the year that contains a link for each day (only need to run one time per year)
-# NPRPageParser.NPRArticleLinkCacheCreator(2018) # 1996 - 2020
+# Create a json file for the year that contains a link for each day (only need to run one time per year)
+NPRPageParser.NPRArticleLinkCacheCreator(2021) # 1996 - 2020
