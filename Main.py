a636ba244cdbf12655339366166303d63a5a3229
Jeffrey Hofer <jeffrey.d.hofer@pm.me>
2021-08-15 18:59:28 -0500

    5th through the 15th playlists created

--- Main.py
+++ Main.py
@@ -12,13 +12,13 @@ from NPRPlaylistCreator import NPRPlaylistCreator
 # TODO come up with some way to automate this daily
 
 # # Set all playlists to public
-# startDate = datetime(2021, 8, 4)
+# startDate = datetime(2021, 8, 5)
 # timeDelta = timedelta()
 # nprPlaylistCreator = NPRPlaylistCreator()
-# nprPlaylistCreator.ChangePlaylistToPublic(startDate, datetime(2021, 8, 4), timedelta)
+# nprPlaylistCreator.ChangePlaylistToPublic(startDate, datetime(2021, 8, 15), timedelta)
 
 # # Used to parse a range of dates, load the json for those days, and make playlists on spotify
-# startDate = datetime(2021, 8, 4) # check 9 to 3, 28 for missing track playlists
+# startDate = datetime(2021, 8, 5) # check 9 to 3, 28 for missing track playlists
 # projectName = "MoWeEd"
 # weekendEdition = "Weekend Edition"
 # morningEdition = "Morning Edition"
@@ -27,7 +27,7 @@ from NPRPlaylistCreator import NPRPlaylistCreator
 # nprPageParser = NPRPageParser()
 # spotifyTracks = list()
 # startTime = datetime.now()
-# while startDate == datetime(2021, 8, 4):
+# while startDate >= datetime(2021, 8, 5):
 #     processedTime = datetime.now()
 #     projectPath = projectName + " Article Data/{0}/{1}/".format(startDate.year, startDate.strftime("%m"))
 #     morningEditionFileName = projectName + " {0} {1} {2}".format(startDate.strftime("%Y-%m-%d"), startDate.strftime("%a"), "Morning Edition")
@@ -120,7 +120,7 @@ from NPRPlaylistCreator import NPRPlaylistCreator
 # for month, daylinks in editionYearLinkCache.items():
 #     for idx, link in enumerate(daylinks):
 #         if int(month) == 8:
-#             if idx >= 0:
+#             if idx == 4:
 #                 nprSpotifySearch = NPRSpotifySearch()
 #                 nprPlaylistCreator = NPRPlaylistCreator()
 #                 nprPageParser = NPRPageParser()
