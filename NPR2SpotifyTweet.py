21552a8d8c6e22cd91d6d9d39bcdd9d2dbd6a7d3
Sockemboffer <Sockemboffer@users.noreply.github.com>
2021-10-16 13:25:35 -0500

    Created NPR Weekend Edition playlist for October 16th, 2021 on Spotify.

--- NPR2SpotifyTweet.py
+++ NPR2SpotifyTweet.py
@@ -1,3 +1,5 @@
-"@MoWeEd2Spotify" "Green heart #NPR2Spotify created a playlist for NPR Morning Edition October 15th, 2021. ❤️🌎📻 #Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists "
+"@MoWeEd2Spotify"
 
-"Created NPR Morning Edition playlist for October 15th, 2021 on Spotify."
+"#NPR2Spotify created a playlist for NPR Morning Edition October 16th, 2021. ❤️🌎📻 #Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists"
+
+"Created NPR Morning Edition playlist for October 16th, 2021 on Spotify."
