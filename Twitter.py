15f024ecd13a65c227306b124cadc06ee61c05fc
Sockemboffer <jeppyh@gmail.com>
2022-01-14 09:55:17 -0600

    Playlists created for January 10th - 14th.

---
+++ Twitter.py
@@ -0,0 +1,5 @@
+"Created a playlists for NPR Morning and Weekend Editions from November 22nd through January 8th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
+
+"Created a playlists for NPR Weekend Edition for January 9th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
+
+"Created an NPR Morning Edition playlist for January 10th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
