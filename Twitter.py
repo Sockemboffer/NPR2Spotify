30a79abd379ddab16c16fe1148783523aa69348a
Sockemboffer <binarybeard.com>
2022-01-24 08:23:44 -0600

    Playlists for Jan 18th through 24th.

--- Twitter.py
+++ Twitter.py
@@ -1,4 +1,4 @@
-"Created a playlists for NPR Morning and Weekend Editions from November 22nd through January 8th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
+"Created playlists for NPR Morning and Weekend Editions from November 22nd through January 8th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
 
 "Created a playlists for NPR Weekend Edition for January 9th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
 
