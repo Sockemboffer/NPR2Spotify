74cc5f1fe0e82c85b52ebe8b1d04c6c8dec641ba
Sockemboffer <binarybeard.com>
2022-02-03 09:11:45 -0600

    Missing playlists data

--- Twitter.py
+++ Twitter.py
@@ -1,5 +1,18 @@
-"Created playlists for NPR Morning and Weekend Editions from November 22nd through January 8th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
+"1 MW" "Created a playlist for NPR Morning and Weekend Edition November 22nd through January 8th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
 
-"Created a playlists for NPR Weekend Edition for January 9th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
+"1 WM" "Created a playlist for NPR Weekend and Morning Edition November 22nd through January 8th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
 
-"Created an NPR Morning Edition playlist for January 10th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
+"1 W" "Created a playlist for NPR Weekend Edition January 22nd and January 8th, 2022. ❤️🌎📻#Donate #NPR #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
+
+"1 M" "Created a playlist for NPR Morning Edition for January 22nd, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #VaccinesWork #SpotifyPlaylists #Spotify"
+
+"2 Ws" "Created playlists for NPR Weekend Edition January 29th and January 30th, 2022. ❤️🌎📻#Donate #NPR #WeekendEdition #VaccinesWork #SpotifyPlaylists #Spotify"
+
+"2 Ms" "Created playlists for NPR Morning Edition for January 22nd through January 30th, 2022. ❤️🌎📻#Donate #NPR #MorningEdition #VaccinesWork #SpotifyPlaylists #Spotify"
+
+"1 date week day"               "a playlist ... for"
+"1 date weekend day"            "a playlist ... for"
+"2 dates weekend days"          "playlists for ... and"
+"2 dates week days"             "playlists for ... and"
+"2+ dates week days"            "playlists for ... through"
+"2+ dates weekend week days"    "playlists for ... through"
